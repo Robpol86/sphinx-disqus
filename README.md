414e4f66cfb04c140dcc6e6e4e6aa625385b5067
Robpol86 <robpol86@gmail.com>
2021-06-07 18:21:50 -0500

    Using Black for consistent code style. (#9)
    Also compacting GitHub Actions workflow file.

--- README.md
+++ README.md
@@ -9,6 +9,7 @@ Embed [Disqus](https://disqus.com) comments in Sphinx documents/pages.
 [![Github-CI][github-ci]][github-link]
 [![Coverage Status][codecov-badge]][codecov-link]
 [![Documentation Status][rtd-badge]][rtd-link]
+[![Code style: black][black-badge]][black-link]
 [![PyPI][pypi-badge]][pypi-link]
 [![PyPI Downloads][pypi-dl-badge]][pypi-dl-link]
 
@@ -18,6 +19,8 @@ Embed [Disqus](https://disqus.com) comments in Sphinx documents/pages.
 [codecov-link]: https://codecov.io/gh/Robpol86/sphinx-disqus
 [rtd-badge]: https://readthedocs.org/projects/sphinx-disqus/badge/?version=latest
 [rtd-link]: https://sphinx-disqus.readthedocs.io/en/latest/?badge=latest
+[black-badge]: https://img.shields.io/badge/code%20style-black-000000.svg
+[black-link]: https://github.com/ambv/black
 [pypi-badge]: https://img.shields.io/pypi/v/sphinx-disqus.svg
 [pypi-link]: https://pypi.org/project/sphinx-disqus
 [pypi-dl-badge]: https://img.shields.io/pypi/dw/sphinx-disqus?label=pypi%20downloads
--- tests/unit_tests/test_identifier.py
+++ tests/unit_tests/test_identifier.py
@@ -19,35 +19,35 @@ disqus_shortname = 'good'
 """
 
 PARAMS = [
-    ('Some Sample Title', 'Some Sample Title\n=================\n\n', ''),  # Only title has the identifier.
-    ('Hi!', '===\nHi!\n===\n\n', ''),  # Another way to specify the title.
-    ("&quot;Hmm...&amp;", 'Old Title\n=========\n\n', '    :disqus_identifier: "Hmm...&'),  # Override the title.
-    ('The Title', '', '    :disqus_identifier: The Title'),  # No title, option set instead.
-    ('', '', ''),  # No identifier set and no title.
+    ("Some Sample Title", "Some Sample Title\n=================\n\n", ""),  # Only title has the identifier.
+    ("Hi!", "===\nHi!\n===\n\n", ""),  # Another way to specify the title.
+    ("&quot;Hmm...&amp;", "Old Title\n=========\n\n", '    :disqus_identifier: "Hmm...&'),  # Override the title.
+    ("The Title", "", "    :disqus_identifier: The Title"),  # No title, option set instead.
+    ("", "", ""),  # No identifier set and no title.
 ]
 
 
-@pytest.mark.parametrize('expected,rst_title,option', PARAMS)
+@pytest.mark.parametrize("expected,rst_title,option", PARAMS)
 def test(monkeypatch, tmpdir, expected, rst_title, option):
     """Test valid and invalid values."""
-    tmpdir.join('conf.py').write(BASE_CONFIG.format(py.path.local(__file__).join('..', '..')))
-    tmpdir.join('conf.py').write('', mode='a')
-    tmpdir.join('index.rst').write('{}.. toctree::\n    :maxdepth: 2\n.. disqus::\n{}'.format(rst_title, option))
-    monkeypatch.setattr(directives, '_directives', getattr(directives, '_directives').copy())
-    monkeypatch.setattr(roles, '_roles', getattr(roles, '_roles').copy())
+    tmpdir.join("conf.py").write(BASE_CONFIG.format(py.path.local(__file__).join("..", "..")))
+    tmpdir.join("conf.py").write("", mode="a")
+    tmpdir.join("index.rst").write("{}.. toctree::\n    :maxdepth: 2\n.. disqus::\n{}".format(rst_title, option))
+    monkeypatch.setattr(directives, "_directives", getattr(directives, "_directives").copy())
+    monkeypatch.setattr(roles, "_roles", getattr(roles, "_roles").copy())
 
     srcdir = confdir = str(tmpdir)
-    outdir = tmpdir.join('_build', 'html')
-    doctreedir = outdir.join('doctrees').ensure(dir=True, rec=True)
-    app = application.Sphinx(srcdir, confdir, str(outdir), str(doctreedir), 'html')
+    outdir = tmpdir.join("_build", "html")
+    doctreedir = outdir.join("doctrees").ensure(dir=True, rec=True)
+    app = application.Sphinx(srcdir, confdir, str(outdir), str(doctreedir), "html")
 
     if expected:
         app.builder.build_all()
-        html_body = outdir.join('index.html').read()
+        html_body = outdir.join("index.html").read()
         disqus_div = re.findall(r'(<div[^>]+ id="disqus_thread"[^>]*></div>)', html_body)[0]
         assert 'data-disqus-identifier="{}"'.format(expected) in disqus_div
         return
 
     with pytest.raises(DisqusError) as exc:
         app.builder.build_all()
-    assert exc.value.args[0] == 'No title nodes found in document, cannot derive disqus_identifier config value.'
+    assert exc.value.args[0] == "No title nodes found in document, cannot derive disqus_identifier config value."
